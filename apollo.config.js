c79e047f470c1d16f47f04cbba4f96b9cd0d2583
Drake Costa <drake@saeris.io>
2020-03-19 12:44:07 -0700

    Fix Apollo Config Endpoint URL

--- apollo.config.js
+++ apollo.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   service: {
-		endpoint: {
-			name: `launchpad-lk3jvvvjnq`,
-      url: `http://localhost:1337/dev`
+    endpoint: {
+      name: `launchpad-lk3jvvvjnq`,
+      url: `http://localhost:1337/${
+        process.env.NETLIFY ? `.netlify/functions/mini-movie-db-api/` : `dev`
+      }`
     }
   }
 };
